39ea8e7c3293b668818577acb539d8b567fe954d
Jose Alvarez <alvarezloaiciga@gmail.com>
2017-05-26 14:12:30 -0600

    Weird FizzBuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,2 +1,22 @@
 class FizzBuzz
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def run
+    array_range = (1..@limit)
+    array_range.each do |iterator|
+      if (iterator % 3) == 0 && (iterator % 3) == 0
+        puts "FizzBuzz"
+      elsif (iterator % 3) == 0
+        puts "Fizz"
+      elsif (iterator % 2) == 0
+        puts "Buzz"
+      else
+        puts iterator
+      end
+    end
+  end
 end
+
+FizzBuzz.new(10).run
